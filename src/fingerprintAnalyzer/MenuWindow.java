36ef30b246b387e88c42a66a29224336e24fef87
JackNeus <jack.neus@gmail.com>
2014-11-05 19:03:25 -0500

    Started File Selection

--- src/fingerprintAnalyzer/MenuWindow.java
+++ src/fingerprintAnalyzer/MenuWindow.java
@@ -1,20 +1,26 @@
 package fingerprintAnalyzer;
 
+import java.io.File;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class MenuWindow extends Application {
-	public void start(Stage primaryStage){
+	public void start(final Stage primaryStage){
+		final File file1, file2;
+		
 		primaryStage.setTitle("Select fingerprints");
 		
 		GridPane grid = new GridPane();
@@ -23,29 +29,50 @@ public class MenuWindow extends Application {
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
 		
+		//Submit Box
 		Button btn = new Button("OK");
 		HBox hbBtn = new HBox(10);
 		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
 		hbBtn.getChildren().add(btn);
 		grid.add(hbBtn, 2, 4);
 		
-		Scene scene = new Scene(grid, 300, 150);
-		primaryStage.setScene(scene);
-		
+		//Title Box
 		Text scenetitle = new Text("Choose files:");
 		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 		grid.add(scenetitle, 0, 0, 2, 1);
+
+		final FileChooser fileChooser = new FileChooser();
+		final Text fileName1 = new Text();
+		grid.add(fileName1, 1, 1);
 		
-		Label print1 = new Label("Fingerprint 1: ");
-		grid.add(print1, 0, 1);
+		Button choose1 = new Button ("Fingerprint 1");
+		choose1.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent e){
+				File file = fileChooser.showOpenDialog(primaryStage);
+				if(file != null){
+					fileName1.setText(file.getName());
+				}
+			}
+		});
+		grid.add(choose1, 0, 1);
+	
+		final Text fileName2 = new Text();
+		grid.add(fileName2, 1, 2);
 		
-		//File Chooser
+		Button choose2 = new Button ("Fingerprint 2");
+		choose2.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent e){
+				File file = fileChooser.showOpenDialog(primaryStage);
+				if(file != null){
+					fileName2.setText(file.getName());
+				}
+			}
+		});
+		grid.add(choose2, 0, 2);
 		
-		Label print2 = new Label("Fingerprint 2: ");
-		grid.add(print2, 0, 2);
-		
-		//File Chooser
-		
+		//Misc. 
+		Scene scene = new Scene(grid, 400, 150);
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
